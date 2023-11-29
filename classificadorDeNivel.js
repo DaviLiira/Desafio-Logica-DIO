f3b0c08926347f2f9d2ca09de2520453e9f78b3e
DaviLiira <davilira444@gmail.com>
2023-11-29 01:03:09 -0300

    Alteração de If, else if para Switch case

--- classificadorDeNivel.js
+++ classificadorDeNivel.js
@@ -2,30 +2,31 @@ let nomeDoHeroi = "Zigfried"
 let xpDoHeroi = parseInt(Math.random() * 20000) + 1;
 console.log(xpDoHeroi)
 
-
-    if (xpDoHeroi < 1000)
+    switch(true){
+    case xpDoHeroi < 1000:
         console.log("Rank atual do " + nomeDoHeroi + " é Ferro.")
-    else if (xpDoHeroi > 1000 && xpDoHeroi < 2001)
+        break
+    case xpDoHeroi > 1000 && xpDoHeroi < 2001:
         console.log("Rank atual do " + nomeDoHeroi + " é Bronze.")
-        
-    else if (xpDoHeroi > 2000 && xpDoHeroi < 5001)
+        break  
+    case xpDoHeroi > 2000 && xpDoHeroi < 5001:
         console.log("Rank atual do " + nomeDoHeroi + " é Prata.")
-        
-    else if (xpDoHeroi > 5000 && xpDoHeroi < 7001)
+        break      
+    case xpDoHeroi > 5000 && xpDoHeroi < 7001:
         console.log("Rank atual do " + nomeDoHeroi + " é Ouro.")
-        
-    else if (xpDoHeroi > 7000 && xpDoHeroi < 8001)
+        break       
+    case xpDoHeroi > 7000 && xpDoHeroi < 8001:
         console.log("Rank atual do " + nomeDoHeroi + " é Platina.")
-        
-    else if (xpDoHeroi > 8000 && xpDoHeroi < 9001)
+        break        
+    case xpDoHeroi > 8000 && xpDoHeroi < 9001:
         console,log("Rank atual do " + nomeDoHeroi + " é Diamante.")
-        
-    else if (xpDoHeroi > 9000 && xpDoHeroi < 10001)
-        console.log("Rank atual do " + nomeDoHeroi + " é Ascendente.")    
-        
-    else if  (xpDoHeroi > 10000 && xpDoHeroi < 12001)
+        break       
+    case xpDoHeroi > 9000 && xpDoHeroi < 10001:
+        console.log("Rank atual do " + nomeDoHeroi + " é Ascendente.")
+        break   
+    case  xpDoHeroi > 10000 && xpDoHeroi < 12001:
         console.log("Rank atual do " + nomeDoHeroi + " é Imortal.")
-        
-    else
+        break   
+    default:
         console.log("Rank atual do " + nomeDoHeroi + " é Radiante. Paranbéns, você chegou ao rank maximo")
-    
+}
